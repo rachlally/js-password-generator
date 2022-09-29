f91bf82704cd9cac7b8448a8bbc218202ca1f09e
Dahlia <rlally89@gmail.com>
2022-09-29 10:01:07 -0700

    password length parameter works

--- Develop/script.js
+++ Develop/script.js
@@ -7,11 +7,11 @@ var result = []
 //Create arrays of uppercase, lowercase, numbers, and special characters
 var lowerAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numberOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacter = ["!", "@", "#", "$", "%", "&", "*"];
 
-//based on the responses trim/build available pool of characters
-var criteriaArray = [""]
+//Based on the responses build available pool of characters
+var criteriaArray = [""];
 
 // Write password to the #password input
 function writePassword() {
@@ -27,35 +27,42 @@ function generatePassword() {
   alert("Select your password criteria");
 
   //prompt user for length
-  //check length is between 8-128 characters
+  //check length is between 8-128 characters by checking for invalid lengths
   var desiredLength = prompt("Desired length between 8-128 characters:");
-    if (desiredLength >= 8 || desiredLength <= 128);
-      var result = desiredLength.concat;
-
+    if (desiredLength < 8 || desiredLength > 128) {
+      alert("Invalid length");
+      generatePassword() 
+     // return;
+    }
   
-
-  //store the responses (series of booleans)
+ 
+  //prompt user for other desired criteria
+  //store responses in single variable
   var lowerCase = confirm("Include lowercase?");
     if (lowerCase == true) {
-      var result = lowerAlphabet.concat;
+      var criteriaArray = criteriaArray.concat(lowerAlphabet);
     } 
   var upperCase = confirm("Include uppercase?");
     if (upperCase == true) {
-      var result = upperAlphabet.concat;
+      var criteriaArray = criteriaArray.concat(upperAlphabet);
     } 
   var numbers = confirm("Include numbers?");
     if (numbers == true) {
-      var result = numberOptions.concat;
+      var criteriaArray = criteriaArray.concat(numberOptions);
     } 
   var symbols = confirm("Include special characters? (RECOMMENDED)");
   if (symbols == true) {
-    var result = specialCharacter.concat;
+    var criteriaArray = criteriaArray.concat(specialCharacter);
   } 
 
   //build password character by character
+// for loop
+
   //generate random int within range of available chars length
   //add char to result
   //do until length = passwordLength
+
+  //function password = 
 
   return result
 }
